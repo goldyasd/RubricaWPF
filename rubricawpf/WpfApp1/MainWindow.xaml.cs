128007067af40d4f43572ea62b4aa38b37c23775
Mattia Montemaggi <67807749+goldyasd@users.noreply.github.com>
2024-02-07 08:13:48 +0100

    Add files via upload

---
+++ rubricawpf/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    using Accessibility;
+    using System;
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Persona> persone = new List<Persona>();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            List<Contatto> contatti = new List<Contatto>();
+            
+            StreamReader sr = new StreamReader("dati.csv");
+            StreamReader sr1 = new StreamReader("Persona.csv");
+            sr1.ReadLine();
+            sr.ReadLine();
+            while (!sr.EndOfStream)
+            {
+                string stringa = sr.ReadLine(); 
+                if(stringa.Split(';').Length == 3)
+                {
+                    
+                    contatti.Add(new Contatto(stringa));
+
+                }
+            }
+            for(int i = contatti.Count + 1; i <= 100; i++)
+            {
+                    contatti.Add(new Contatto(i));
+            }
+            while (!sr1.EndOfStream)
+            {
+                string s = sr1.ReadLine();
+                persone.Add(new Persona(Convert.ToInt32(s.Split(';')[0]), Enum.Parse<Type>(s.Split(';')[1]), s.Split(';')[2]));
+            }
+            dg.ItemsSource = contatti;
+
+        }
+
+
+        private void dg_LoadingRow(object sender, DataGridRowEventArgs e)
+        {
+            Contatto c = e.Row.Item as Contatto;
+            if (c.Nome == null && c.Cognome == null)
+            {
+                    e.Row.Background = Brushes.Red;
+                    e.Row.Foreground = Brushes.White;
+            }
+        }
+
+        private void dg1_LoadingRow(object sender, SelectionChangedEventArgs e)
+        {
+            Contatto pe = e.AddedItems[0] as Contatto;
+            List<Persona> list = new List<Persona>();
+            foreach(Persona p in persone)
+            {
+                try
+                {
+                    if (p.IdPersona == pe.N)
+                    {
+                        list.Add(p);
+                    }
+                }
+                catch { }
+                
+            }
+            dg1.ItemsSource = list;
+        }
+    }
+}
---
+++ rubricawpf/WpfApp1/Persona.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    enum Type
+    {
+        Email,
+        Telefono,
+        Nome,
+        Cognome,
+        Web
+    }
+    internal class Persona
+    {
+        private int idPersona;
+        private Type tipo;
+        private string valore;
+
+        public Persona(int idPersona, Type tipo, string valore)
+        {
+            this.idPersona = idPersona;
+            this.tipo = tipo;
+            this.valore = valore;
+        }
+
+        public int IdPersona
+        {
+            get { return idPersona; }
+            set { idPersona = value; }
+        }
+
+        public Type Tipo
+        {
+            get { return tipo; }
+        }
+
+        public string Valore
+        {
+            get { return valore; }
+        }
+    }
+}
